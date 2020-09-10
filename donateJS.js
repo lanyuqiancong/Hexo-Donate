c08c7a401beb4e8b1b1e0e4157c98ed49dbe4be9
lanyuqiancong <69624701+lanyuqiancong@users.noreply.github.com>
2020-09-10 16:45:09 +0800

    Update donateJS.js

--- donateJS.js
+++ donateJS.js
@@ -5,7 +5,7 @@
  * Last Update: 2020/6/4
  */
 //通过get请求，获取getJsonData.php文件从数据库中获取的数据
-$.get("http://yxrxar1515.m.awhost.cn/getJsonData.php", function getData(data) {
+$.get("https://donate.qiancong.me/getJsonData.php", function getData(data) {
     //强转jsSon类型，便于操作数据
     var donationInfo = JSON.parse(data);
     //获取table标签，将获取的数据插入
